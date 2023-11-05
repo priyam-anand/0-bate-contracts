5e8faa1765cb7701e98d2fb556e96a992b4320eb
priyam-anand <priyam27.anand@gmail.com>
2023-11-05 22:07:28 +0530

    feat: added sc for basic end to end flow

--- __test__/WillExec.test.ts
+++ __test__/WillExec.test.ts
@@ -1,18 +1,26 @@
 import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import { algos, getOrCreateKmdWalletAccount } from '@algorandfoundation/algokit-utils';
+import algosdk, { Algodv2, AtomicTransactionComposer, bigIntToBytes } from 'algosdk';
+import * as algokit from '@algorandfoundation/algokit-utils';
+import fs from 'fs';
+import path from 'path';
 import { WillexecClient } from '../contracts/clients/WillexecClient';
 
 const fixture = algorandFixture();
 
 let appClient: WillexecClient;
-
+let sender: algosdk.Account;
+const abi = JSON.parse(fs.readFileSync(path.join(__dirname, '../contracts/artifacts/Willexec.abi.json'), 'utf8'));
+const contract = new algosdk.ABIContract(abi);
+let algod: Algodv2;
 describe('Willexec', () => {
   beforeEach(fixture.beforeEach);
 
   beforeAll(async () => {
     await fixture.beforeEach();
-    const { algod, testAccount } = fixture.context;
-
+    const { testAccount, kmd } = fixture.context;
+    algod = fixture.context.algod;
     appClient = new WillexecClient(
       {
         sender: testAccount,
@@ -21,21 +29,88 @@ describe('Willexec', () => {
       },
       algod
     );
-
+    sender = await getOrCreateKmdWalletAccount(
+      {
+        name: 'currentSender2',
+        fundWith: algos(10000),
+      },
+      algod,
+      kmd
+    );
     await appClient.create.createApplication({});
   });
 
-  test('sum', async () => {
-    const a = 13;
-    const b = 37;
-    const sum = await appClient.doMath({ a, b, operation: 'sum' });
-    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
-  });
+  test('createWill ', async () => {
+    const from = sender.addr;
+    // const algodClient = new algosdk.Algodv2(
+    //   'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    //   'http://localhost',
+    //   4001
+    // );
+    // console.log(await algodClient.accountInformation((await appClient.appClient.getAppReference()).appAddress).do());
+    await appClient.appClient.fundAppAccount(algokit.microAlgos(100000));
 
-  test('difference', async () => {
-    const a = 13;
-    const b = 37;
-    const diff = await appClient.doMath({ a, b, operation: 'difference' });
-    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+    const asset1 = 'SLUIOR5APCXTY25URFQTXYJQPJAEKPMOTLCXZYLTHCZ2TGKUYNY22Z7LPA';
+    const asset2 = 'IT7GSYW4SL2NOODDDAZCOFDDKFL7VBCKCD6TBFUYTS2SCKYIYQSN2CZKRA';
+    const assetspk = algosdk.decodeAddress(asset1);
+    const assetpk2 = algosdk.decodeAddress(asset2);
+    const assetsToPass = [...assetspk.publicKey].concat([...assetpk2.publicKey]) as unknown as Uint8Array;
+
+    const amount1 = bigIntToBytes(123321, 8);
+    const amount2 = bigIntToBytes(1111, 8);
+    const amountsToPass = [...amount1].concat([...amount2]) as unknown as Uint8Array;
+
+    const toToPass = [...assetpk2.publicKey].concat([...assetspk.publicKey]) as unknown as Uint8Array;
+
+    const COST_PER_BYTE = 400;
+    const COST_PER_BOX = 2500;
+    const MAX_BOX_SIZE = 8192;
+
+    const totalCost =
+      COST_PER_BOX + // cost of box
+      MAX_BOX_SIZE * COST_PER_BYTE + // cost of data
+      64 * COST_PER_BYTE + // cost of key
+      64 * COST_PER_BYTE + // cost of key
+      123321 +
+      1111;
+    const payTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from: sender.addr,
+      to: (await appClient.appClient.getAppReference()).appAddress,
+      amount: algokit.microAlgos(totalCost).valueOf(),
+      suggestedParams: await algokit.getTransactionParams(undefined, algod),
+    });
+
+    const senderSIgner = algosdk.makeBasicAccountTransactionSigner(sender);
+
+    const atc = new AtomicTransactionComposer();
+    const boxes: algosdk.BoxReference[] = new Array(8).fill({
+      appIndex: Number((await appClient.appClient.getAppReference()).appId),
+      name: bigIntToBytes(1, 8),
+    });
+
+    atc.addMethodCall({
+      appID: (await appClient.appClient.getAppReference()).appId as number,
+      method: contract.getMethodByName('createWill'),
+      methodArgs: [
+        from,
+        amountsToPass,
+        amountsToPass,
+        toToPass,
+        amountsToPass,
+        toToPass,
+        Math.floor(Date.now() / 1000),
+        { txn: payTxn, signer: senderSIgner },
+      ],
+      sender: sender.addr,
+      signer: senderSIgner,
+      suggestedParams: await algokit.getTransactionParams(undefined, algod),
+      boxes,
+    });
+
+    const result = await atc.execute(algod, 4);
+    console.log(result.methodResults[0].returnValue);
+
+    const res = await appClient.increateEndTime({ willId: 1 }, { sender, boxes });
+    console.log('res ', res);
   });
 });
--- contracts/WillExec.algo.ts
+++ contracts/WillExec.algo.ts
@@ -1,47 +1,147 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
+type Will = {
+  from: Address;
+  id: number;
+  assets: bytes;
+  assetsAmount: bytes;
+  assetsTo: bytes;
+  nativeAmount: bytes;
+  nativeTo: bytes;
+  endTime: number;
+};
+
+const COST_PER_BYTE = 400;
+const COST_PER_BOX = 2500;
+const MAX_BOX_SIZE = 8192;
+const ONE_DAY = 86400;
+
 // eslint-disable-next-line no-unused-vars
 class Willexec extends Contract {
-  /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: number, b: number): number {
-    return a + b;
+  wills = BoxMap<uint64, Will>();
+
+  currentId = GlobalStateKey<uint64>({ key: 'id' });
+
+  assetsHelper = GlobalStateMap<Asset, uint64>({ maxKeys: 8 });
+
+  createWill(
+    from: Address,
+    assets: bytes,
+    assetsAmount: bytes,
+    assetsTo: bytes,
+    nativeAmount: bytes,
+    nativeTo: bytes,
+    endTime: number
+  ): uint64 {
+    // verify native txn amounts etc
+    let rounds = len(nativeAmount) / 8;
+    assert(rounds === len(nativeTo) / 32);
+    let totalCost =
+      COST_PER_BOX + // cost of box
+      MAX_BOX_SIZE * COST_PER_BYTE + // cost of data
+      64 * COST_PER_BYTE + // cost of key
+      64 * COST_PER_BYTE; // cost of keys
+
+    for (let i = 0; i < rounds; i = i + 1) {
+      const currentAmountInBytes = extract3(nativeAmount, i * 8, 8);
+      totalCost = totalCost + btoi(currentAmountInBytes);
+    }
+
+    verifyTxn(this.txnGroup[0], { receiver: this.app.address, amount: { greaterThanEqualTo: totalCost } });
+    // send these assets to a dex and store the output ????
+
+    // verify asset txns
+    rounds = len(assetsAmount) / 8;
+    assert(rounds === len(assets) / 8 && rounds === len(assetsTo) / 32);
+
+    for (let i = 0; i < rounds; i = i + 1) {
+      const currentAssetInBytes = extract3(assets, i * 8, 8);
+      const currentAmountInBytes = extract3(assets, i * 8, 8);
+
+      const currentAsset = Asset.fromID(btoi(currentAssetInBytes));
+      const currentAmount = btoi(currentAmountInBytes);
+
+      if (this.assetsHelper(currentAsset).exists) {
+        const value = this.assetsHelper(currentAsset).value + currentAmount;
+        this.assetsHelper(currentAsset).value = value;
+      } else {
+        this.assetsHelper(currentAsset).value = currentAmount;
+      }
+    }
+
+    for (let i = 0; i < rounds; i = i + 1) {
+      const currentAssetInBytes = extract3(assets, i * 8, 8);
+      const currentAsset = Asset.fromID(btoi(currentAssetInBytes));
+
+      const value = this.assetsHelper(currentAsset).value;
+      // verify asset transfers from this group
+
+      verifyTxn(this.txnGroup[i + 1], {
+        xferAsset: currentAsset,
+        assetAmount: { greaterThanEqualTo: value },
+        assetReceiver: this.app.address,
+      });
+
+      // send these assets to a dex and store the output ????
+      this.assetsHelper(currentAsset).delete();
+    }
+
+    assert(endTime > globals.latestTimestamp);
+
+    // store the will
+    this.currentId.value = this.currentId.value + 1;
+
+    const will: Will = {
+      from: from,
+      id: this.currentId.value,
+      assets: assets,
+      assetsAmount: assetsAmount,
+      assetsTo: assetsTo,
+      nativeAmount: nativeAmount,
+      nativeTo: nativeTo,
+      endTime: endTime,
+    };
+
+    this.wills(this.currentId.value).value = will;
+    return this.currentId.value;
   }
 
-  /**
-   * Calculates the difference between two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
-   */
-  private getDifference(a: number, b: number): number {
-    return a >= b ? a - b : b - a;
+  increateEndTime(willId: number): void {
+    const will = this.wills(willId).value;
+    assert(this.txn.sender === will.from);
+    const currentEndTime = this.wills(willId).value.endTime;
+    this.wills(willId).value.endTime = currentEndTime + ONE_DAY * 365; // increase time by 1 year
   }
 
-  /**
-   * A method that takes two numbers and does either addition or subtraction
-   *
-   * @param a The first number
-   * @param b The second number
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
-   */
-  doMath(a: number, b: number, operation: string): number {
-    let result: number;
+  executeWill(willId: number): void {
+    const will = this.wills(willId).value;
 
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
+    assert(globals.latestTimestamp >= will.endTime);
 
-    return result;
+    // transfer algos
+    let rounds = len(will.nativeAmount) / 8;
+    for (let i = 0; i < rounds; i = i + 1) {
+      const currentAmount = btoi(extract3(will.nativeAmount, i * 8, 8));
+      const currentTo = Address.fromBytes(extract3(will.nativeTo, i * 32, 32));
+
+      sendPayment({
+        amount: currentAmount,
+        receiver: currentTo,
+      });
+    }
+
+    // transfer assets
+    rounds = len(will.assets) / 32;
+    for (let i = 0; i < rounds; i = i + 1) {
+      const currentAsset = Asset.fromID(btoi(extract3(will.assets, i * 32, 32)));
+      const currentTo = Address.fromBytes(extract3(will.assetsTo, i * 32, 32));
+      const currentAmount = btoi(extract3(will.assetsAmount, i * 8, 8));
+
+      sendAssetTransfer({
+        xferAsset: currentAsset,
+        assetAmount: currentAmount,
+        assetReceiver: currentTo,
+      });
+    }
   }
 }
